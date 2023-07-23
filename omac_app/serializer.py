8674573ec13e5ff04ef976e91175c22c35107560
rauf.akchurin <abdurauf.dev@gmail.com>
2023-07-24 00:02:20 +0500

    with last validation

--- omac_app/serializer.py
+++ omac_app/serializer.py
@@ -65,6 +65,7 @@ class WeightingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Weighting
         fields = (
+            "id",
             "animal",
             "date",
             "weight",
@@ -74,6 +75,11 @@ class WeightingSerializer(serializers.ModelSerializer):
         animal = data.get('animal')
         date = data.get('date')
 
+        # If it's an update, exclude the current instance from the uniqueness check
+        instance = self.instance
+        if instance and instance.animal == animal and instance.date == date:
+            return data
+
         if Weighting.objects.filter(animal=animal, date=date).exists():
-            raise serializers.ValidationError("One weighting to animal per day")
+            raise serializers.ValidationError("One weighting to this animal per day")
         return data
